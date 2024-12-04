8853d14417ce36d19b7d042c5dc64dc124e2adb7
Atharva Hambarde <atharva.hambarde@cctech.co.in>
2024-12-04 12:45:33 +0530

    Updated

--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -1,94 +1,103 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Paint
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Point previousPoint; // Store the previous point to draw a line
+        private bool _isDrawing; // Flag to check if we are currently drawing
+        private Line _currentLine; // To hold the current line while drawing
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+    
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
             DrawingCanvas.Children.Clear();
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
+        // mouse down event to start drawing
         private void DrawingCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            P1 = e.GetPosition(DrawingCanvas);
-            e.GetPosition(this);
-
+            previousPoint = e.GetPosition(DrawingCanvas); // Get the position where the mouse was pressed
+            _isDrawing = true; 
         }
 
-        Point P1;
+       
+        private void DrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isDrawing) return; 
+
+            var currentPoint = e.GetPosition(DrawingCanvas); 
+
+           
+            var selectedBrushSize = BrushSize.SelectedItem as ComboBoxItem;
+            var brushSizeValue = selectedBrushSize?.Content.ToString();
+            int brushSize = int.TryParse(brushSizeValue, out var size) ? size : 2; 
+
+           
+            var selectedBrushColor = BrushColor.SelectedItem as ComboBoxItem;
+            string brushColor = selectedBrushColor?.Content.ToString();
+            var color = Colors.Black; 
+            switch (brushColor)
+            {
+                case "Red":
+                    color = Colors.Red;
+                    break;
+                case "Green":
+                    color = Colors.Green;
+                    break;
+                case "Yellow":
+                    color = Colors.Yellow;
+                    break;
+                case "Blue":
+                    color = Colors.Blue;
+                    break;
+                case "Black":
+                    color = Colors.Black;
+                    break;
+            }
+
+            // Draw a line if the mouse is moving
+            _currentLine = new Line
+            {
+                X1 = previousPoint.X,
+                Y1 = previousPoint.Y,
+                X2 = currentPoint.X,
+                Y2 = currentPoint.Y,
+                StrokeThickness = brushSize,
+                Stroke = new SolidColorBrush(color)
+            };
+
+            // Add the line to the canvas
+            DrawingCanvas.Children.Add(_currentLine);
+
+            // Update the previous point for the next drawing step
+            previousPoint = currentPoint;
+        }
+
+       
+        private void DrawingCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _isDrawing = false; 
+        }
+
         private void DrawingCanvas_MouseLeave(object sender, MouseEventArgs e)
         {
-            var P2 = e.GetPosition(DrawingCanvas);
-            DrawingCanvas.Children.Add(new Line()
-            {
-                X1 = P1.X,
-                Y1 = P1.Y,
-                X2 = P2.X,
-                Y2 = P2.Y,
-                StrokeThickness = 3,
-                Stroke = new SolidColorBrush { Color = Colors.Blue }
-            });
-
-            //Line line = new Line();
-            //Thickness thickness = new Thickness(101, -11, 362, 250);
-            //line.Margin = thickness;
-            //line.Visibility = System.Windows.Visibility.Visible;
-            //line.StrokeThickness = 4;
-            //line.Stroke = System.Windows.Media.Brushes.Black;
-            //line.X1 = 10;
-            //line.X2 = 40;
-            //line.Y1 = 70;
-            //line.Y2 = 70;
-
-            //DrawingCanvas.Children.Add(line);
-
+            _isDrawing = false; 
         }
 
-      
-
-
+    
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
     }
 }
